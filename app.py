aa259bb8192e0f002780d7c2cace567489a241c1
timoteahu <51691041+timoteahu@users.noreply.github.com>
2022-10-27 17:32:02 -0700

    c

--- app.py
+++ app.py
@@ -9,6 +9,13 @@ app = Flask('app')
 def index():
     return render_template("/index.html")
 
+@app.route('/mission')
+def mission():
+    return render_template("/mission.html")
 
+@app.route('/launchhacks')
+def launchhacks():
+    return render_template("launchhacks.html")
+    
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=1000, debug=True)
