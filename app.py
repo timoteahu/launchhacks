83b69dd8403c91d35524bdff23555608b8e563c6
timoteahu <51691041+timoteahu@users.noreply.github.com>
2022-10-27 18:23:50 -0700

    c

--- app.py
+++ app.py
@@ -9,19 +9,19 @@ if 'DYNO' in os.environ:
 
 @app.route('/')
 def index():
-    return render_template("/index.html")
+    return render_template("/index.html", page="Home")
 
 @app.route('/mission')
 def mission():
-    return render_template("/mission.html")
+    return render_template("/mission.html", page="Mission")
 
 @app.route('/launchhacks')
 def launchhacks():
-    return render_template("launchhacks.html")
+    return render_template("launchhacks.html", page="LaunchHacks I")
 
 @app.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template("about.html", page="About")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=1000, debug=True)
